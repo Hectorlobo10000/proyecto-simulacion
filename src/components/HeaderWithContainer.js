8175df42015d3a25529a0d796393da9ec87b8ad2
Hectorlobo10000 <hector.lobo10000@gmail.com>
2019-12-10 15:26:10 -0600

    ahorro dolares y lempiras BO

--- src/components/HeaderWithContainer.js
+++ src/components/HeaderWithContainer.js
@@ -74,17 +74,25 @@ export default function ElevateAppBar(props) {
 
   const [state, setState] = React.useState({
     saveMonth: 0,
+    saveMonthD: 0,
     years: 0,
-    futures: [],
+    futuresL: [],
+    futuresD: [],
   });
 
   const handlerSaveMonth = event => {
     setState({ ...state, saveMonth: parseFloat(event.target.value, 2) });
   }
 
+  const handlerSaveMonthD = event => {
+    setState({ ...state, saveMonthD: parseFloat(event.target.value, 2) });
+  }
+
   const handlerYears = event => {
     setState({ ...state, years: parseFloat(event.target.value, 2) * 12 });
   }
+
+
 
   const math = (addSaveMonth, saveMonthInitial, i, ti) => {
     const rate = parseFloat((ti / 360).toFixed(8), 8); //tasa nominal diaria
@@ -98,8 +106,6 @@ export default function ElevateAppBar(props) {
       const newSaveMonth = parseFloat((addSaveMonth + interest).toFixed(2), 2); //nuevo capital
       addSaveMonth = parseFloat((newSaveMonth + saveMonthInitial).toFixed(2));
     }
-
-    //future.push(addSaveMonth);
     return addSaveMonth;
   }
 
@@ -108,105 +114,99 @@ export default function ElevateAppBar(props) {
     var future = [];
     var addSaveMonth = state.saveMonth; //ahorro por mes, entrada del usuario
     var saveMonthInitial =  state.saveMonth;
+
     for (var i = 1; i <= state.years; i ++) { //numero de anos del ahorro
       if (saveMonthInitial >= 500 && saveMonthInitial <= 50000) {
         
         const aSM = math(addSaveMonth, saveMonthInitial, i, 0.035);
-        addSaveMonth = aSM
+        addSaveMonth = aSM;
         future.push(aSM);
 
       } else if (saveMonthInitial >= 50000.01 && saveMonthInitial <= 100000) {
         
         const aSM = math(addSaveMonth, saveMonthInitial, i, 0.045);
-        addSaveMonth = aSM
+        addSaveMonth = aSM;
         future.push(aSM);
 
       } else if (saveMonthInitial >= 100000.01 && saveMonthInitial <= 200000) {
         
         const aSM = math(addSaveMonth, saveMonthInitial, i, 0.05);
-        addSaveMonth = aSM
+        addSaveMonth = aSM;
         future.push(aSM);
 
       } else if (saveMonthInitial >= 200000.01 && saveMonthInitial <= 500000) {
         
         const aSM = math(addSaveMonth, saveMonthInitial, i, 0.0575);
-        addSaveMonth = aSM
+        addSaveMonth = aSM;
         future.push(aSM);
 
       } else if (saveMonthInitial >= 500000.01 && saveMonthInitial <= 1000000) {
         
         const aSM = math(addSaveMonth, saveMonthInitial, i, 0.0650);
-        addSaveMonth = aSM
+        addSaveMonth = aSM;
         future.push(aSM);
 
       } else if (saveMonthInitial >= 1000000.01 && saveMonthInitial <= 2000000) {
         
         const aSM = math(addSaveMonth, saveMonthInitial, i, 0.0675);
-        addSaveMonth = aSM
+        addSaveMonth = aSM;
         future.push(aSM);
 
       } else if (saveMonthInitial >= 2000000.01 && saveMonthInitial <= 5000000) {
         
         const aSM = math(addSaveMonth, saveMonthInitial, i, 0.0725);
-        addSaveMonth = aSM
+        addSaveMonth = aSM;
         future.push(aSM);
 
       } else if (saveMonthInitial >= 5000000.01 && saveMonthInitial <= 10000000) {
         
         const aSM = math(addSaveMonth, saveMonthInitial, i, 0.0750);
-        addSaveMonth = aSM
+        addSaveMonth = aSM;
         future.push(aSM);
 
       } else if (state.saveMonth >= 10000000.01) {
         
         const aSM = math(addSaveMonth, saveMonthInitial, i, 0.08);
-        addSaveMonth = aSM
+        addSaveMonth = aSM;
         future.push(aSM);
 
       }
     }
-    setState({ ...state, futures: future });
-  }
 
+    var futureD = [];
+    var addSaveMonthD = state.saveMonthD; 
+    var saveMonthInitialD =  state.saveMonthD;
 
-/*   const calculator = () => {
-    var future = [];
-    var addSaveMonth = state.saveMonth; //ahorro por mes, entrada del usuario
-    var saveMonthInitial =  state.saveMonth;
-    for (var i = 1; i <= state.years; i ++) { //numero de anos del ahorro
-      if (saveMonthInitial >= 500 && saveMonthInitial <= 50000) {
-        const rate = parseFloat((0.035 / 360).toFixed(8), 8); //tasa nominal diaria
-        if (i === 1) {
-          const interest = parseFloat((addSaveMonth * rate * 30).toFixed(2), 2); //intereses
-          const newSaveMonth = parseFloat((addSaveMonth + interest).toFixed(2), 2); //nuevo capital
-          addSaveMonth = newSaveMonth;
-        }
-        else {
-          const interest = parseFloat(((addSaveMonth + saveMonthInitial) * rate * 30).toFixed(2), 2); //intereses
-          const newSaveMonth = parseFloat((addSaveMonth + interest).toFixed(2), 2); //nuevo capital
-          addSaveMonth = parseFloat((newSaveMonth + saveMonthInitial).toFixed(2));
-        }
+    for (var j = 1; j <= state.years; j++) { //numero de anos del ahorro
+      if (saveMonthInitialD >= 250 && saveMonthInitialD <= 1000) {
+        
+        const aSM = math(addSaveMonthD, saveMonthInitialD, j, 0.012);
+        addSaveMonthD = aSM;
+        futureD.push(aSM);
 
-        future.push(addSaveMonth);
-      } else if (state.saveMonth >= 50000.01 && state.saveMonth <= 100000) {
-        const rate = parseFloat((0.045 / 360).toFixed(8), 8); //tasa nominal diaria
-        if (i === 1) {
-          const interest = parseFloat((addSaveMonth * rate * 30).toFixed(2), 2); //intereses
-          const newSaveMonth = parseFloat((addSaveMonth + interest).toFixed(2), 2); //nuevo capital
-          addSaveMonth = newSaveMonth;
-        }
-        else {
-          const interest = parseFloat(((addSaveMonth + saveMonthInitial) * rate * 30).toFixed(2), 2); //intereses
-          const newSaveMonth = parseFloat((addSaveMonth + interest).toFixed(2), 2); //nuevo capital
-          addSaveMonth = parseFloat((newSaveMonth + saveMonthInitial).toFixed(2));
-        }
+      } else if (saveMonthInitialD >= 1000.01 && saveMonthInitialD <= 50000) {
+        
+        const aSM = math(addSaveMonthD, saveMonthInitialD, j, 0.0190);
+        addSaveMonthD = aSM;
+        futureD.push(aSM);
 
-        future.push(addSaveMonth);
+      } else if (saveMonthInitialD >= 50000.01 && saveMonthInitialD <= 150000) {
+        
+        const aSM = math(addSaveMonthD, saveMonthInitialD, j, 0.0275);
+        addSaveMonthD = aSM;
+        futureD.push(aSM);
+
+      } else if (saveMonthInitialD >= 150000.01) {
+        
+        const aSM = math(addSaveMonthD, saveMonthInitialD, j, 0.035);
+        addSaveMonthD = aSM;
+        futureD.push(aSM);
+
       }
     }
-    setState({ ...state, futures: future });
-  } */
-
+    setState({ ...state, futuresL: future, futuresD: futureD });
+    //setState({ ...state, futuresD: futureD });
+  }
 
   return (
     <React.Fragment>
@@ -225,11 +225,20 @@ export default function ElevateAppBar(props) {
           <TextField
             fullWidth
             id="saveMonth"
-            label="Cantidad ahorro mensual"
+            label="Cantidad ahorro mensual en lempiras"
             variant="outlined"
             type="number"
             defaultValue={0}
             onChange={handlerSaveMonth}
+          />
+          <TextField
+            fullWidth
+            id="saveMonthD"
+            label="Cantidad ahorro mensual en dolares"
+            variant="outlined"
+            type="number"
+            defaultValue={0}
+            onChange={handlerSaveMonthD}
           />
           <TextField
             fullWidth
@@ -264,9 +273,16 @@ export default function ElevateAppBar(props) {
               <Grid item xs={4}>
                 <Paper className={classes.paper}>
                   <Typography variant="h5" align="center" className={classes.title}>Banco Occidente</Typography>
+                  <Typography variant="body1" align="left">Cuenta de ahorro en lempiras</Typography>
                   {
-                    state.futures.map((value, index) => (
+                    state.futuresL.map((value, index) => (
                       <Typography key={value} variant="body2" align="left">{index + 1}. Interes mas ahorro mensual: {String(value)} LPS.</Typography>
+                    ))
+                  }
+                  <Typography variant="body1" align="left">Cuenta de ahorro en Dolares</Typography>
+                  {
+                    state.futuresD.map((value, index) => (
+                      <Typography key={value} variant="body2" align="left">{index + 1}. Interes mas ahorro mensual: {String(value)} USD.</Typography>
                     ))
                   }
                 </Paper>
